65f64f84b5fcbff3c52eb3920f844d980e3c7fd7
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-21 21:25:13 -0300

    feat: Adicionando Spring boot no projeto.

---
+++ cognito/src/main/java/com/unifsa_eng_soft_20261/cognito/CognitoApplication.java
@@ -0,0 +1,13 @@
+package com.unifsa_eng_soft_20261.cognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoApplication.class, args);
+	}
+
+}
---
+++ cognito/src/test/java/com/unifsa_eng_soft_20261/cognito/CognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifsa_eng_soft_20261.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
